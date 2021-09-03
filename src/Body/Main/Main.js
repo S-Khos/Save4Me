9f392663f71518bd368bfc4702fb1582e5bdc54b
Shayan-Bravo <shayan.khosravi@ontariotechu.net>
2021-09-02 20:14:23 -0400

    updated body

--- src/Body/Main/Main.js
+++ src/Body/Main/Main.js
@@ -1,17 +1,38 @@
 import './Main.css';
-import { FaDownload } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 import { useEffect, useState } from "react";
 import DownloadButton from './DownloadButton';
 import SwitchSelector from "react-switch-selector";
+import $ from "jquery"
 
 function Body() {
   const [videoUrl, setVideoUrl] = useState("");
   const [videoId, setVideoId] = useState("");
-  const [downloadType, setDownloadType] = useState("");
+  const [propId, setPropId] = useState("");
+  const [downloadType, setDownloadType] = useState("mp3");
+  const [search, setSearch] = useState(false);
 
   useEffect(() => {
-    const id = videoUrl.replace("https://www.youtube.com/watch?v=", '');
-    setVideoId(id);
+    var inputArea = document.getElementById('link-input');
+    if (videoUrl.includes("https://www.youtube.com/watch?v=")){
+      $(inputArea).removeClass('invalid-link');
+      $(inputArea).removeClass('regular-link');  
+      $(inputArea).addClass('valid-link');
+      const id = videoUrl.replace("https://www.youtube.com/watch?v=", '');
+      if (id !== "" || !id.includes(" ")){
+        setVideoId(id);
+        console.log(id);
+      }
+    } else if (videoUrl !== "") {
+      $(inputArea).removeClass('regular-link');
+      $(inputArea).removeClass('valid-link');
+      $(inputArea).addClass('invalid-link');
+      console.log("invalid");
+    } else if (videoUrl === ""){
+      $(inputArea).removeClass('invalid-link');
+      $(inputArea).removeClass('valid-link');
+      $(inputArea).addClass('regular-link');
+    }
   },[videoUrl]);
 
   const options = [
@@ -28,32 +49,43 @@ function Body() {
         selectedFontColor: "#005bbc",
     }
   ];
+
   const onChange = (newValue) => {
     console.log(newValue);
     setDownloadType(newValue);
   };
 
-  const initialSelectedIndex = options.findIndex(({value}) => value === "mp3");
-  
+  function submit(){
+    if (videoId !== ""){
+      console.log(search);
+      setPropId(videoId);
+      setSearch(true);
+    } else {
+      console.log("empty video id");
+    }
+  }  
   return (
     <div className="Body">
       <div className="body-container">
           <form className="input-form" onSubmit={e => e.preventDefault()}>
               <input value={videoUrl} onChange={e => setVideoUrl(e.target.value)} type="text" id="link-input" placeholder="Paste valid YouTube link here..."/>
-              <SwitchSelector id="type-selector" 
-              fontSize={40} 
-              onChange={onChange} 
-              options={options} 
-              initialSelectedIndex={initialSelectedIndex} 
-              backgroundColor={"#005bbc"} 
-              fontColor={"white"}
-              optionBorderRadius={6}
-              wrapperBorderRadius={6}
-              />
+              <a id="input-submit" onClick={e => submit()} >
+                <FaSearch id="download-logo"/>
+              </a>
+              <div id="type-selector">
+                <SwitchSelector  
+                fontSize={40} 
+                onChange={onChange}
+                options={options}
+                backgroundColor={"#005bbc"} 
+                fontColor={"white"}
+                optionBorderRadius={6}
+                wrapperBorderRadius={6}
+                />
+              </div>
           </form>
           <div className="downloadButton">
-            {/* change type to selector */}
-            <DownloadButton id={videoId} type="mp3"></DownloadButton>
+            {search ? <DownloadButton id={propId} type={downloadType}/> : null}
           </div>
       </div>
     </div>
